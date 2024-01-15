99a5e3c3eafb06fbf38afc893b0320907f058ccf
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-01-15 23:49:27 +0530

    update: completed header component

--- know/app/components/Header.tsx
+++ know/app/components/Header.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import SearchButton from "./icons/SearchButton";
+import BurgerMenu from "./icons/BurgerMenu";
+
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center">
       <div className="text-2xl font-medium py-5">know.</div>
       <div className="flex justify-between items-center">
-        <div className="font-light text-sm">Search</div>
-        
-
-        <div className="ml-10">burger</div>
+        <div className="font-light text-sm flex items-center border-[1px] rounded-lg py-1.5 px-[2rem]">Search
+          <SearchButton />
+        </div>
+        <div className="ml-10">
+          <BurgerMenu />
+        </div>
       </div>
+    
     </header>
   );
 };
