a3c8c8c5b7dcb734e50ebb94bdfdd8a439e6c4a9
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-01-15 22:46:58 +0530

    todo: ideal/best structure for website template

--- know/app/components/Canvas.tsx
+++ know/app/components/Canvas.tsx
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 
 const Canvas = () => {
   return (
-    <div className="h-screen w-full px-[5rem] bg-[#1A1C1A] text-[#FDFEFD]">
+    <div className="h-[100vh] w-full px-[5rem] bg-[#1A1C1A] text-[#FDFEFD]">
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
       <Header />
       <About />
