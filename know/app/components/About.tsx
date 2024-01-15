b5ef1ee07f8b2a877672d221b47edec9a03ce414
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-01-16 01:19:27 +0530

    update: completed store link button

--- know/app/components/About.tsx
+++ know/app/components/About.tsx
@@ -1,29 +1,31 @@
 import React from "react";
+import AppStoreButton from "./icons/AppStoreButton";
+import PlayStoreButton from './icons/PlayStoreButton'
 
-const About = () => {
+const About:React.FC = () => {
   return (
     <div className="flex mt-8">
-
-
       <div className="flex flex-col justify-between w-[60%]">
-        <div className="text-4xl">
+        <div className="text-5xl font-semibold">
           know. - this is a numerous collection of various articles
         </div>
-        <h3 className="text-xl mt-16">Go to arcticles {">"}</h3>
+        <h3 className="text-xl mt-16 border-b-[1px] w-max">Go to arcticles {">"}</h3>
       </div>
-
 
       <div className="flex flex-col justify-between w-[40%]">
         <div className="font-light">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum harum, voluptate in magnam dolorum reprehenderit modi culpa sed molestiae quam dolore neque non quae fuga eaque, consequuntur maiores omnis iusto.
+          <p>Electronic scientific library of open access. The site catalog is regularly overgrown with texts of articles from various scientific publications. Grouped by journals and headings. Designed by Abhijay Rajvansh</p>
         </div>
         <div className="flex">
-          <div>google</div>
-          <div>apple</div>
+          <div>
+            <AppStoreButton />
+          </div>
+          <div className="ml-5">
+            <PlayStoreButton />
+          </div>
+          
         </div>
       </div>
-
-
     </div>
   );
 };
