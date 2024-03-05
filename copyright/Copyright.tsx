c8281d851bfb2b545a95eeba142b7b26861a027d
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-03-06 02:39:55 +0530

    update: optimized copyright.tsx

--- copyright/Copyright.tsx
+++ copyright/Copyright.tsx
@@ -4,7 +4,7 @@ export default function Copyright () {
   return (
     <section className="mt-10 flex">
       {/* target="_blank" rel="noopener noreferrer" */}
-      <Link isExternal href='https://linkedin.com/in/abhijayrajvansh'>
+      <Link  href='https://linkedin.com/in/abhijayrajvansh' target="_blank" rel="noopener noreferrer">
         <p className="text-white font-medium">© Abhijay Rajvansh</p>
       </Link>
       <p>, All Right Reserved.</p>
